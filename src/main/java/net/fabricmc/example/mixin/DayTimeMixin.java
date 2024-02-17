719714774ef19d1107129f141a25b90c80133ed8
Felix Grohme <felix@grohme.de>
2024-02-17 16:41:18 +0100

    Daytime working now

--- src/main/java/net/fabricmc/example/mixin/DayTimeMixin.java
+++ src/main/java/net/fabricmc/example/mixin/DayTimeMixin.java
@@ -6,22 +6,27 @@ import net.minecraft.client.world.ClientWorld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientWorld.Properties.class)
 public abstract class DayTimeMixin {
-    private static long timeOfDay = 6000L;
-    private static boolean enableTime = true;
+
+    @Shadow private long timeOfDay;
+    private static Logger LOG = LoggerFactory.getLogger(DayTimeMixin.class);
+
+    private static long setTimeOfDay = 6000L;
+    private static boolean enableTime = false;
 
     @Inject(at = @At("RETURN"), method = "getTimeOfDay", cancellable = true)
-    @Environment(EnvType.CLIENT)
     public void getTimeOfDay(CallbackInfoReturnable<Long> cir) {
         if (enableTime) {
-            cir.setReturnValue(timeOfDay);
+            cir.setReturnValue(setTimeOfDay);
         } else {
-            cir.cancel();
+            cir.setReturnValue(cir.getReturnValue());
         }
     }
 }
