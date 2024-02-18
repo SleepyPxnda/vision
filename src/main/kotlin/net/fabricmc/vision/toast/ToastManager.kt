ba533a0e1b2c32016ff48f6dad29c7349dba5d9f
Felix Grohme <felix@grohme.de>
2024-02-18 20:58:54 +0100

    More comments and pictures

--- src/main/kotlin/net/fabricmc/vision/toast/ToastManager.kt
+++ src/main/kotlin/net/fabricmc/vision/toast/ToastManager.kt
@@ -9,26 +9,43 @@ class ToastManager {
 
     var visionToast: VisionToast = VisionToast(Text.of(""),Text.of(""));
 
+    /**
+     * Adds a title to the toast
+     */
     fun title(title: Text): ToastManager {
         visionToast.title = title;
         return this;
     }
 
+    /**
+     * Adds a description to the toast
+     */
     fun description(description: Text): ToastManager {
         visionToast.description = description;
         return this;
     }
 
+    /**
+     * Adds a custom duration to the toast
+     */
     fun duration(duration: Double): ToastManager {
         visionToast.duration = duration;
         return this;
     }
 
+    /**
+     * Creates the toast with a given type
+
+     */
     fun type(type: ToastType): VisionToast {
         visionToast.type = type;
         return visionToast;
     }
 
+    /**
+     * Creates the toast with a custom texture
+     * Icon should be 64x64 but gets cropped down to 16x16
+     */
     fun texture(texture: Identifier): VisionToast {
         visionToast.texture = texture;
         return visionToast;
