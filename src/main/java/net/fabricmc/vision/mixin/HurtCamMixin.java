1db6e362983d5ec9e979b0f9fddd15ec7e401741
Felix Grohme <felix@grohme.de>
2024-02-19 20:47:38 +0100

    Added HurtCamMixin

---
+++ src/main/java/net/fabricmc/vision/mixin/HurtCamMixin.java
@@ -0,0 +1,24 @@
+package net.fabricmc.vision.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public abstract class HurtCamMixin {
+    boolean noHurtCam = false;
+    boolean modifiedHurtCam = false;
+
+    @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
+    public void onHurtViewTilt(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        if (noHurtCam) ci.cancel();
+    }
+
+    @Inject(method = "bobView", at = @At("HEAD"), cancellable = true)
+    public void onViewBob(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        if (noHurtCam) ci.cancel();
+    }
+}
